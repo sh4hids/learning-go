aef5dd307f0a1f534b0790074c0f5b177f7b73a0
sh4hids <hello@shahid.pro>
2018-08-22 22:32:17 +0600

    Initial commit

---
+++ 02-variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+  var power int
+  power  = 9000
+
+  var age int = 25
+  currentYear := 2018
+
+  fmt.Printf("It's over %d\n", power)
+  fmt.Printf("My age %d\n", age)
+  fmt.Printf("It's %d\n", currentYear)
+}
---
+++ 03-function.go
@@ -0,0 +1,10 @@
+package main
+
+func main()  {
+  age := getMyAge()
+  println("I'm", age, "years old");
+}
+
+func getMyAge() int  {
+  return 25
+}
