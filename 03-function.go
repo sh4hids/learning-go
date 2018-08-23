eadbbaf23ead67c08f97c7bb151383691e7e8931
sh4hids <hello@shahid.pro>
2018-08-23 08:41:19 +0600

    Learned variable assignment

--- 03-function.go
+++ 03-function.go
@@ -1,6 +1,6 @@
 package main
 
-func main()  {
+func main() {
   age := getMyAge()
   println("I'm", age, "years old");
 }
---
+++ 04-variable-assigment.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	age := 20
+	name, age := "Shahid", 25 // at least one variable should be new
+	println("I'm", name, "and I'm", age, "years old")
+}
